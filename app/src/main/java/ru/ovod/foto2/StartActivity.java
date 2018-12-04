4440017355e3f369fc8a86b5b9d56fc8fca19767
Григорий <grisha@gkuznetsov.ru>
2018-12-04 22:22:02 +0300

    Восстановил Toolbar на главном окне.

--- app/src/main/java/ru/ovod/foto2/StartActivity.java
+++ app/src/main/java/ru/ovod/foto2/StartActivity.java
@@ -44,8 +44,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         verifyStoragePermissions(this);
         dbhelper = new DBHelper(getApplicationContext());
