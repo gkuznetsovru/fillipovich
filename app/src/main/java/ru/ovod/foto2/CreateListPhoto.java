53bed30c3f62788e16cf4d5367ff4f93672d6731
Григорий <grisha@gkuznetsov.ru>
2018-12-05 00:37:17 +0300

    Начал делать работу  с фото - вариант 2.

---
+++ app/src/main/java/ru/ovod/foto2/CreateListPhoto.java
@@ -0,0 +1,32 @@
+package ru.ovod.foto2;
+
+public class CreateListPhoto {
+    private String image_title;
+    private String filename;
+    private Integer image_id;
+
+    public String getImage_title() {
+        return image_title;
+    }
+
+    public void setImage_title(String image_title) {
+        this.image_title = image_title;
+    }
+
+    public Integer getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(Integer image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+}
